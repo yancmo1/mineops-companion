94d10a82fd327a9b8f9d647d2a7ec67a857ff5e5
Default User <default@example.com>
2025-12-22 08:14:13 -0600

    Add MineOpsLayout itemSpacing

--- MineOpsCompanionPackage/Sources/MineOpsCompanionFeature/Theme/LayoutConstants.swift
+++ MineOpsCompanionPackage/Sources/MineOpsCompanionFeature/Theme/LayoutConstants.swift
@@ -5,4 +5,5 @@ public enum MineOpsLayout {
     public static let cornerRadius: CGFloat = 12
     public static let cardPadding: CGFloat = 16
     public static let sectionSpacing: CGFloat = 20
+    public static let itemSpacing: CGFloat = 12
 }
